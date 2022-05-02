3556be08d3cce867eb3c28211b92275717dd096f
andriisukhodolskiy <63231425+andriisukhodolskiy@users.noreply.github.com>
2022-05-02 15:53:37 +0300

    Homework1

---
+++ Lesson1/Task1.java
@@ -0,0 +1,31 @@
+package Lesson1;
+
+import java.io.*;
+/*
+In method main() write code for solving next tasks:
+Define integer variables a and b. Read values a and b from Console and calculate: 
+a + b 
+a - b 
+a * b 
+a / b. 
+Output obtained results.
+*/
+public class Task1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader rb = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter number a: ");
+		String aString = rb.readLine();
+		int a = Integer.parseInt(aString);
+		System.out.print("Enter number b: ");
+		String bString = rb.readLine();
+		double b = Integer.parseInt(bString);
+		
+		System.out.println("Result a + b: " + (a + b));
+		System.out.println("Result a - b: " + (a - b));
+		System.out.println("Result a * b: " + (a * b));
+		System.out.println("Resulr a / b: " + (a / b));
+	}
+		
+
+}
---
+++ Lesson1/Task2.java
@@ -0,0 +1,16 @@
+package Lesson1;
+/*
+ Output question “How are you?“. Define string variable answer. Read the value answer and output: “You are (answer)". 
+
+ */
+import java.io.*;
+public class Task2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("How are you? ");
+		String st = br.readLine();
+		System.out.println("You are " + st);
+	}
+
+}
