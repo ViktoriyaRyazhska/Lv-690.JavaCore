ffc4748226d6a952e07c71f1cd81e06b2987587a
andriisukhodolskiy <andriisukhodolskiy@gmail.com>
2022-05-03 14:34:38 +0300

    Homework1

--- ls1/HomeWorkLs1.java
+++ ls1/HomeWorkLs1.java
@@ -1,4 +1,4 @@
-package ls11;
+package ls1;
 /*
  * Create console application. In method main() write code for solving next tasks:
 1) Flower bed is shaped like a circle. Calculate the perimeter and area by entering the radius. 
