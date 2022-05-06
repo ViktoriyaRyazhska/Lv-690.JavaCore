3188699496c2e4f35ac8b34ef5784ea7f214c72f
YevheniySerdiuk <yevheniyserdiuk@gmail.com>
2022-05-06 20:34:43 +0300

    HW3\Practical tasks

---
+++ HW3/Practical tasks/LessonFour.java
@@ -0,0 +1,47 @@
+package JavaCore;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LessonFour {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String fn1 = printWithInput("Input  first :_",br);
+        String fn2 = printWithInput("Input  second :_",br);
+        String fn3 = printWithInput("Input  third :_",br);
+        int val1 = Integer.parseInt(fn1);
+        int val2 = Integer.parseInt(fn2);
+        int val3 = Integer.parseInt(fn3);
+
+        int oddQuantity=0;
+
+        LessonFour lf = new LessonFour();
+        if (lf.compareVal(val1)==true) {
+            oddQuantity++;
+        }
+        if (lf.compareVal(val2)==true) {
+            oddQuantity++;
+        }
+        if (lf.compareVal(val3)==true) {
+            oddQuantity++;
+        }
+
+        System.out.println("Odd from them:" + oddQuantity);
+
+    }
+    public boolean compareVal(int val){
+
+        if (val % 2 != 0) {
+            return  true;}
+        else
+            return false;
+    }
+    public static String printWithInput(String txt, BufferedReader br)throws IOException {
+        System.out.println(txt);
+        return br.readLine();
+
+}
+}
