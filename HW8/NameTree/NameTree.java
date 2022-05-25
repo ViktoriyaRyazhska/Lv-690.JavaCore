0aa154eaf5b693d23cb147f632480331ae651812
s.nesternko <sawsoer1@gmail.com>
2022-05-25 14:18:37 +0300

    HW8U3

--- HW8/NameTree/NameTree.java
+++ HW8/NameTree/NameTree.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class NameTree {
     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
         TreeMap<String, String> NameTree = new TreeMap<>();
         int i = 0;
         int treeIndex = 7;
@@ -18,22 +19,28 @@ public class NameTree {
 
         System.out.println(NameTree);
 
-
         for (Map.Entry<String, String> e : NameTree.entrySet()) {
             if (NameTree.containsKey(e.getKey())) {
                 i++;
             }
         }
-
         if (i == treeIndex) {
             System.out.println("All name are different");
         } else {
             System.out.println("You have same name");
         }
 
+        System.out.println("Input name what you want remove");
+        String name = sc.next();
 
-        NameTree.remove("Aron");
+        if(NameTree.containsKey(name)){
+            System.out.println("User " + name + " removed");
+            NameTree.remove(name);
+        }
+
+
         System.out.println(NameTree);
+
 
     }
 }
