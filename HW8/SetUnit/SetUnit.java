c36233dd0707330f307a265c7c54753c6c3c5181
s.nesternko <sawsoer1@gmail.com>
2022-05-25 13:38:26 +0300

    HW9U

--- HW8/SetUnit/SetUnit.java
+++ HW8/SetUnit/SetUnit.java
@@ -1,6 +1,7 @@
 package HomeWork.SetUnit;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class SetUnit {
     public static void main(String[] args) {
@@ -13,20 +14,40 @@ public class SetUnit {
         set1.add(6);
 
         HashSet<Integer> set2 = new HashSet<>();
-        set2.add(2);
-        set2.add(3);
-        set2.add(4);
-        set2.add(5);
         set2.add(6);
         set2.add(7);
         set2.add(8);
         set2.add(9);
+        set2.add(10);
+        set2.add(11);
+        set2.add(12);
+        set2.add(13);
 
-        System.out.println(set1);
-        set1.addAll(set2);
-        System.out.println(set1);
-        set1.retainAll(set2);
-        System.out.println(set1);
+        System.out.println(union(set1,set2));
+        System.out.println(intersect(set1, set2));
 
     }
+
+    public static Set<Integer> union(HashSet<Integer> s1, HashSet<Integer> s2) {
+        HashSet<Integer> union = new HashSet<>();
+        for (Integer i1:s1){union.add(i1);}
+            for (Integer i2:s2){union.add(i2);}
+            return union;
+
+
+    }
+    public static Set<Integer> intersect(HashSet<Integer> s1, HashSet<Integer> s2) {
+
+        Set<Integer> intersect = new HashSet<>();
+        for (Integer i1 : s1) {
+            for (Integer i2 : s2) {
+                if (i1 == i2) {
+                    intersect.add(i1);
+                }
+            }
+        }
+        return intersect;
+    }
+
+
 }
