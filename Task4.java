a76113b1c853009f6f49ee5cc426e766a4d83afb
Nickoss1 <104580008+Nickoss1@users.noreply.github.com>
2022-06-23 18:29:49 +0300

    Add files via upload

--- Task4.java
+++ Task4.java
@@ -1,9 +1,42 @@
-package Team1;
+package Homework8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task4 {
+	private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	public Task4() {
-		// TODO Auto-generated constructor stub
+
+	}
+	
+	public void Execute () {
+		String loudStr = "aeuoiAEUOI";
+		String word = Read("word: ", br);
+		int count = 0;
+		for (int i=0; i<word.length(); i++) {
+			int pos = loudStr.indexOf(word.charAt(i));
+			if (pos>=0) {
+				count = count +1;
+			};
+		}
+		System.out.println("loud letters in word "+ word+ " =" + count);
+		
+	}
+	
+	public static String Read(String q, BufferedReader br) {
+		System.out.println(q);
+		try {
+			return br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+		}
 	}
 
 }
+	
+	
+
+
