22fd2a1a56bcc218fa138f6d322fcb3427f12627
TEMSK1Y <104579880+TEMSK1Y@users.noreply.github.com>
2022-06-23 18:49:44 +0300

    Update main.java

--- Teame2/src/main.java
+++ Teame2/src/main.java
@@ -33,5 +33,56 @@ public class main {
         }
         System.out.println(count);
     }
+    public static int inputTask10() {
+        int number = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the number > 1:");
+        boolean flag = true;
+        while (flag) {
+            try {
+                number = Integer.parseInt(br.readLine());
+                if (number < 1) {
+                    throw new RuntimeException("Incorrect number, try again");
+                } else {
+                    flag = false;
+                    return number;
+                }
+            } catch (IOException | NumberFormatException e) {
+                System.err.println("Incorrect value. Try again");
+            }
+        }
+        return 0;
+    }
+
+    public static void executeTask10() {
+        int steps = 0;
+        int number1 = inputTask10();
+
+        while (number1 != 1) {
+            if (number1 % 2 == 0) {
+                number1 = number1 / 2;
+                steps++;
+            } else {
+                number1 = number1 * 3 + 1;
+                steps++;
+            }
+        }
+        System.out.println(steps);
+    }
+    public static void executeTask11() {
+        int sum = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0) {
+                sum += i;
+            } else {
+                if (i % 5 == 0) {
+                    sum += i;
+                } else {
+                    sum += 0;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
 
 }
